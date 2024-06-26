88224655549600fdcf2f49ab03779bdecc850d87
Abdusshh <abdullah.gules@simpplr.com>
2024-06-26 17:42:23 +0300

    Url names fixed for deployment.

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import schedule_email_view, send_email_view
 
 urlpatterns = [
-    path('schedule-email/', schedule_email_view, name='schedule-email'),
-    path('send-email/', send_email_view, name='send-email'),
+    path('schedule-email', schedule_email_view, name='schedule-email'),
+    path('send-email', send_email_view, name='send-email'),
 ]
