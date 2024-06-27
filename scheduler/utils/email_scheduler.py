65ad5483b59e9f36abdce9c3c31ca0469ce9d3cc
Abdusshh <abdullah.gules@simpplr.com>
2024-06-27 17:07:47 +0300

    Cronjob and date selection added.

--- scheduler/utils/email_scheduler.py
+++ scheduler/utils/email_scheduler.py
@@ -1,3 +1,4 @@
+import re
 from upstash_qstash import Client
 from .helpers import get_env_variable
 
@@ -8,3 +9,30 @@ def schedule_email(email_data, delay):
         "body": email_data,
         "delay": delay,
     })
+
+
+
+# schedule email with cron job
+def schedule_email_cronjob(email_data, cron_string):
+    client = Client(get_env_variable("QSTASH_TOKEN"))
+    schedules = client.schedules()
+    response = schedules.create({
+        "destination": "https://email-scheduler-dun.vercel.app/scheduler/send-email",  # Update with your server's URL
+        "cron": cron_string,
+        "body": email_data
+    })
+    return response
+
+CRON_REGEX = re.compile(r'^(?#minute)(\*|(?:[0-9]|(?:[1-5][0-9]))(?:(?:\-[0-9]|\-(?:[1-5][0-9]))?|(?:\,(?:[0-9]|(?:[1-5][0-9])))*)) (?#hour)(\*|(?:[0-9]|1[0-9]|2[0-3])(?:(?:\-(?:[0-9]|1[0-9]|2[0-3]))?|(?:\,(?:[0-9]|1[0-9]|2[0-3]))*)) (?#day_of_month)(\*|(?:[1-9]|(?:[12][0-9])|3[01])(?:(?:\-(?:[1-9]|(?:[12][0-9])|3[01]))?|(?:\,(?:[1-9]|(?:[12][0-9])|3[01]))*)) (?#month)(\*|(?:[1-9]|1[012]|JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)(?:(?:\-(?:[1-9]|1[012]|JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC))?|(?:\,(?:[1-9]|1[012]|JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC))*)) (?#day_of_week)(\*|(?:[0-6]|SUN|MON|TUE|WED|THU|FRI|SAT)(?:(?:\-(?:[0-6]|SUN|MON|TUE|WED|THU|FRI|SAT))?|(?:\,(?:[0-6]|SUN|MON|TUE|WED|THU|FRI|SAT))*))$')
+
+def is_valid_cron(cron_string):
+    return bool(CRON_REGEX.match(cron_string))
+
+if __name__ == '__main__':
+    email_data = {
+        'to_email': 'test',
+        'subject': 'test',
+        'content': 'test'
+    }
+    cron_string = '* * * * *'
+    print(schedule_email_cronjob(email_data, cron_string))
--- scheduler/views.py
+++ scheduler/views.py
@@ -1,29 +1,77 @@
+import re
 from django.shortcuts import render
-from django.http import JsonResponse, HttpResponseRedirect
-from django.urls import reverse
+from django.http import JsonResponse
+from datetime import datetime
 from django.views.decorators.csrf import csrf_exempt
 import json
-from .utils.email_scheduler import schedule_email
+from .utils.email_scheduler import schedule_email, schedule_email_cronjob
 from .utils.send_email import send_email
+
+CRON_REGEX = re.compile(r'^(?#minute)(\*|(?:[0-9]|(?:[1-5][0-9]))(?:(?:\-[0-9]|\-(?:[1-5][0-9]))?|(?:\,(?:[0-9]|(?:[1-5][0-9])))*)) (?#hour)(\*|(?:[0-9]|1[0-9]|2[0-3])(?:(?:\-(?:[0-9]|1[0-9]|2[0-3]))?|(?:\,(?:[0-9]|1[0-9]|2[0-3]))*)) (?#day_of_month)(\*|(?:[1-9]|(?:[12][0-9])|3[01])(?:(?:\-(?:[1-9]|(?:[12][0-9])|3[01]))?|(?:\,(?:[1-9]|(?:[12][0-9])|3[01]))*)) (?#month)(\*|(?:[1-9]|1[012]|JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)(?:(?:\-(?:[1-9]|1[012]|JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC))?|(?:\,(?:[1-9]|1[012]|JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC))*)) (?#day_of_week)(\*|(?:[0-6]|SUN|MON|TUE|WED|THU|FRI|SAT)(?:(?:\-(?:[0-6]|SUN|MON|TUE|WED|THU|FRI|SAT))?|(?:\,(?:[0-6]|SUN|MON|TUE|WED|THU|FRI|SAT))*))$')
+
+def is_valid_cron(cron_string):
+    return bool(CRON_REGEX.match(cron_string))
 
 @csrf_exempt
 def schedule_email_view(request):
+    email_scheduled = False
+    
     if request.method == 'POST':
         to_email = request.POST.get('to_email')
         subject = request.POST.get('subject')
         content = request.POST.get('content')
-        delay = int(request.POST.get('delay', 0))
-        
+        schedule_date_str = request.POST.get('schedule_date')
+        cron_string = request.POST.get('cron_string')
+
         email_data = {
             'to_email': to_email,
             'subject': subject,
             'content': content
         }
-        
-        schedule_email(email_data, delay)
-        return HttpResponseRedirect(reverse('schedule-email'))
+
+        if cron_string:
+            # Validate cron string format
+            if not is_valid_cron(cron_string):
+                return render(request, 'schedule_email.html', {
+                    'email_scheduled': email_scheduled,
+                    'error': 'Invalid cron string format. Please enter a valid cron string.'
+                })
+            # Schedule with cron string
+            schedule_email_cronjob(email_data, cron_string)
+            email_scheduled = True
+
+        elif schedule_date_str:
+            # Schedule with specific date and time
+            schedule_date = datetime.strptime(schedule_date_str, '%Y-%m-%dT%H:%M')
+            current_date = datetime.now()
+
+            # Check if schedule date is in the past
+            if schedule_date < current_date:
+                return render(request, 'schedule_email.html', {
+                    'email_scheduled': email_scheduled,
+                    'error': 'Schedule date cannot be in the past.'
+                })
+            
+            # Check if schedule date is in the future more than a week
+            if (schedule_date - current_date).total_seconds() > 604800:
+                return render(request, 'schedule_email.html', {
+                    'email_scheduled': email_scheduled,
+                    'error': 'Schedule date cannot be more than a week in the future for free pricing.'
+                })
+            
+            delay = int((schedule_date - current_date).total_seconds())
+            schedule_email(email_data, delay)
+            email_scheduled = True
+            
+        else:
+            return render(request, 'schedule_email.html', {
+                'email_scheduled': email_scheduled,
+                'error': 'Please provide either a schedule date or a cron string.'
+            })
+
+        return render(request, 'schedule_email.html', {'email_scheduled': email_scheduled})
     
-    return render(request, 'schedule_email.html')
+    return render(request, 'schedule_email.html', {'email_scheduled': email_scheduled})
 
 @csrf_exempt
 def send_email_view(request):
